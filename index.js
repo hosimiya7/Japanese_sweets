6f4ee4b0765b709d84edef618904cd21203e5f9c
hosimiya7 <nerokarurin@gmail.com>
2021-09-16 21:05:55 +0900

    contact

--- index.js
+++ index.js
@@ -16,3 +16,21 @@
       $nav.removeClass( open );
     });
   } )(jQuery);
+
+  jQuery(function() {
+    var pagetop = $('#page_top');
+    pagetop.hide();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 2500) {  //100pxスクロールしたら表示
+            pagetop.fadeIn();
+        } else {
+            pagetop.fadeOut();
+        }
+    });
+    pagetop.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500); //0.5秒かけてトップへ移動
+        return false;
+    });
+});
